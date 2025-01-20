4331cbaac8ec103166c5c2581789b60c893d4a73
flores-97 <floresvictor15@gmail.com>
2025-01-20 08:54:03 -0800

    entered class comments at the top

--- COMP003A.LEctureActivity2/Program.cs
+++ COMP003A.LEctureActivity2/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace COMP003A.LEctureActivity2
+﻿/*AUthor: Victor Flores
+ Course: COMP003A
+Faculty: Jonathon Cruz
+Purpose: A program demonstrating statements, variables, and operators.*/
+namespace COMP003A.LEctureActivity2
 {
     internal class Program
     {
