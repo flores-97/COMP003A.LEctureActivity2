ae63562609e3edbfef95ed1ae91f4d1bcbb3d657
flores-97 <floresvictor15@gmail.com>
2025-01-20 09:11:06 -0800

    completed steps 3.1

--- COMP003A.LEctureActivity2/Program.cs
+++ COMP003A.LEctureActivity2/Program.cs
@@ -8,7 +8,27 @@ namespace COMP003A.LEctureActivity2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //declared ands initialize variables
+            string userName; //variable to store user's name
+            int userAge; //variable to store user's age
+            double productPrice;//used for prodcut price
+            bool isStudent;// to stroe if user is a student (true or false)
+
+            //prompt user to enter their name
+            Console.WriteLine("Enter your name: ");
+            userName = Console.ReadLine(); //read and assign the user's input to userName
+
+            //prompt user to enter the age
+            Console.WriteLine("Enter your age: ");
+            userAge = int.Parse(Console.ReadLine()); // read and assign user's age
+
+            //prompt user to enter product price
+            Console.Write("Enter the price of a product: ");
+            productPrice = double.Parse(Console.ReadLine());//convert and assing the user's input to productPrice
+
+            //prompt user to indicate if they are a student
+            Console.Write("Are you a student? (true/false): ");
+            isStudent = bool.Parse(Console.ReadLine()); //convert and assign the user's input to isStudent
         }
     }
 }
